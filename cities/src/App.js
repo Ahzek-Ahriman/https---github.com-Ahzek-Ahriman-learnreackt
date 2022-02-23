e92cd27ef7d1f16aa30c57bc94a2024f8efb34a7
Maxim Skalkin <maxmlnsim@gmail.com>
2022-02-23 20:31:38 +0300

    changes

---
+++ cities/src/App.js
@@ -0,0 +1,27 @@
+import React from "react"; // импоритруем платформу реакт
+import Info from "./components/info"; //импортируем компоненты т.е. другие js файлы с html кодом
+import Form from "./components/form";
+import Weather from "./components/weather";
+
+const API_KEY = "76848c4b0e72a07fae20169958776e3e"; //API взят с сайта
+
+class App extends React.Component {
+  gettingWeather = async ()=> { // async получение информации асинхронно без обновления страницу
+    const api_url = await fetch(`api.openweathermap.org/data/2.5/weather?q=Moscow.ru&appid=${API_KEY}&units=metric`);// константа для ссылки где получаем json информацию fetch для прохождения 
+    // по всему URL
+    const data = await api_url.json(); // занесение json информации в переменную
+    console.log(data);
+  }
+  render(){
+    return(
+      <div>
+        {/* вызов компонентов */}
+        <Info/> 
+        <Form/>
+        <Weather/>
+      </div>
+    );
+  }
+}
+
+export default App;
